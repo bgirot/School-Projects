c6d6ba3a48603734980bef7ce7795c346023d8cf
taupz <balthazar.girot@gmail.com>
2022-10-04 09:29:34 +0200

    24.3 and 24.4 done

--- TP/TP1/code/tp_24.py
+++ TP/TP1/code/tp_24.py
@@ -1,5 +1,5 @@
 #24.1
-personne_1 = [x for x in range(1,9)]
+personne_1 = [x for x in range(9)]
 parent_1 = [-1,6,8,0,8,7,0,6,6]
 def premiere_generation(p,i):
     if p[i] == 0:
@@ -14,3 +14,21 @@ def nbr_enfant(i):
         if i == x:
             somme += 1
     return somme
+
+#24.3
+def pink(p,i):
+    somme = 0
+    for el in personne_1:
+        if p[el]%2 == 0:
+            somme += 1
+    return somme
+
+#24.4
+def generation(p,i):
+    g1 = i
+    g = 0
+    while g1 != 0:
+        g1 = p[g1]
+        g += 1
+    return g
+generation(parent_1,7)
