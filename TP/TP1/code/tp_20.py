818835f61e14d3694fe3db1bd87fff40cbbb2708
taupz <balthazar.girot@gmail.com>
2022-10-17 18:16:06 +0200

    19 bis and 27 started, 21 fixed

--- TP/TP1/code/tp_20.py
+++ TP/TP1/code/tp_20.py
@@ -3,11 +3,21 @@ def espace(chaine):
         if str(chaine[i]) == ' ':
             final = chaine[i+1:]            #split utilisable
     return final
-print(espace('je suis'))
+
+def relou(li_corps):
+    base = []
+    finito = ""
+    for i in range(len(li_corps)):
+        base.append("De " + str(li_corps[i])+str(", ") + str(espace(li_corps[i]))+str(", ") + str(espace(li_corps[i])))
+    return base
+        
 
 def jean(li_corps):
     for i in range(len(li_corps)):
-        print("Jean Petit qui danse (bis)\nDe" + str(li_corps[i]) + str(" il danse (bis)\nDe ") + str(li_corps[i])+str(", ") + str(espace(li_corps[i]))+str(", ") + str(espace(li_corps[i])) + str("\nAinsi danse Jean Petit.\n"))
+        print("Jean Petit qui danse (bis)\nDe " + str(li_corps[i]) + str(" il danse (bis)"))
+        for j in range(i+1,0,-1):
+            print((relou(['son doigt','sa main','ses cheveux']))[j-1])
+        print("Ainsi danse Jean Petit.\n")
 
 
 jean(['son doigt','sa main','ses cheveux'])
