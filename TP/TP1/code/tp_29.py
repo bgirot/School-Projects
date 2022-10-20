2756db49a43548e92f0002340f06d48f9143ca5d
taupz <balthazar.girot@gmail.com>
2022-10-20 13:37:46 +0200

    ex 29 done clean code imo

--- TP/TP1/code/tp_29.py
+++ TP/TP1/code/tp_29.py
@@ -25,14 +25,13 @@ dict_morse={
     'X':'-..-',
     'Y':'-.--',
     'Z':'--..',
-    ' ':'␣',
+    ' ':'  ',
 }
 
 def traduire_text_morse(dico,chaine):
     finito =''
     for i in range(len(chaine)):
         finito += str(dico.get(chaine[i]))
-        finito += str("␣")
     return finito
 print(traduire_text_morse(dict_morse,"LE CHAT EST NOIR"))
 
@@ -44,8 +43,12 @@ def reverse_dict(dico):
 
 def traduire_morse_vers_texte(dico,chaine):
     finito = ''
-    word = ''
-    for i in range(len(chaine)):
-        pass                                    #Erreur pcq chaque lettre en morse fait plusieurs caractères
+    liste_mots = chaine.split(' ')
+    #Sépare chaque mot puis change les doubles espaces (devenus '' à cause du split) en '  '
+    for i in range(len(liste_mots)):
+        if str(liste_mots[i]) == '':
+            liste_mots[i] = "  "
+    for el in liste_mots:
+        finito += str(dico.get(el))
     return finito
-print(traduire_morse_vers_texte(reverse_dict(dict_morse),".-.. . -.-. .... .- - . ... - -.--- .. .-. "))
+print(traduire_morse_vers_texte(reverse_dict(dict_morse),".-.. .  -.-. .... .- -  . ... -  -. --- .. .-."))
